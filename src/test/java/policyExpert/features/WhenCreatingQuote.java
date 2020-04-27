e25f15442c030746ffa1328cb6cf9f1e54fefba6
Vlad Dinescu <dinescu.vlad241@gmail.com>
2020-04-27 03:57:36 +0100

    Created Test File

---
+++ src/test/java/policyExpert/features/WhenCreatingQuote.java
@@ -0,0 +1,38 @@
+package policyExpert.features;
+
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
+public class WhenCreatingQuote {
+
+    @Managed(uniqueSession = true)
+    WebDriver herBrowser;
+
+    Actor sergey = Actor.named("Sergey");
+
+    @Steps
+    CompleteQuote completeQuote;
+
+    @Steps
+    CompleteForm potentialCustomer;
+
+    @Steps
+    NavigateTo navigateToThePolicyExpertInsurancePage;
+
+    @Test
+    public void shouldBeAbleToCompleteHomeInsuranceForm(){
+
+
+
+        givenThat(sergey).wasAbleTo(navigateToThePolicyExpertInsurancePage);
+        when(sergey).attemptsTo(CompleteQuote.withDetails());
+            //then(sergey).should(seeThat(TheWebPage.title(), containstString("")))
+    }
+}
